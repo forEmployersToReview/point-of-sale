c0c36b405c909a2ae8bd55c8d35a662b105713e4
Elifaz Filho <zafile3@hotmail.com>
2018-01-28 01:52:31 -0800

    Initial commit

---
+++ src/containers/app.js
@@ -0,0 +1,10 @@
+// Modules
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <div className="pos-root">start</div>;
+  }
+}
+
+export default App;
