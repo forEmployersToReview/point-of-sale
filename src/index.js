f569485f9c11557a3d73cb39d13647d85afb7f26
Elifaz Filho <zafile3@hotmail.com>
2018-02-05 23:53:17 -0800

    [feat] Apollo setup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
+// Modules
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+// Style
 import './style.css';
+
+// Components
 import App from './containers/app';
+
+// Application Config
 import registerServiceWorker from './registerServiceWorker';
 
+// Apollo Client
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri:'https://803votn6w7.execute-api.us-west-2.amazonaws.com/dev/public/graphql'
+  }),
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
   document.getElementById('app')
 );
 registerServiceWorker();
